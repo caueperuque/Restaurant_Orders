068be163e0a9d38dc077256ef2035112b3bcd1bb
caue <caue19052003@gmail.com>
2023-11-07 21:05:10 -0300

    feat: tests

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,23 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
-# Req 1
 def test_ingredient():
-    pass
+    # Teste para um ingrediente com restrições conhecidas
+    ingredient = Ingredient("queijo mussarela")
+    ingredient1 = Ingredient("queijo mussarela")
+    ingredient_test = Ingredient("carne")
+    assert ingredient1.name == "queijo mussarela"
+    assert ingredient1.restrictions == {
+        Restriction.ANIMAL_DERIVED, Restriction.LACTOSE
+        }
+
+    # Teste para um ingrediente com restrições desconhecidas
+    ingredient3 = Ingredient("ingrediente_desconhecido")
+    assert ingredient3.restrictions == set()
+    assert repr(ingredient3) == "Ingredient('ingrediente_desconhecido')"
+
+    # Teste para igualdade entre ingredientes
+    assert ingredient1 == ingredient1
+    assert ingredient1 != ingredient_test
+    assert hash(ingredient1) == hash(ingredient)
+    assert hash(ingredient3) != hash(ingredient)
